5a18f349008a5a445eef761caf59e78c179bbcd2
crazygolden <amogh.thakurdesai@gmail.com>
2024-06-07 16:38:55 +0530

    RuntimeError: Given groups=1, weight of size [64, 128, 9, 9], expected input[4, 3, 128, 128] to have 128 channels, but got 3 channels instead

--- data.py
+++ data.py
@@ -6,14 +6,21 @@ import os
 import random
 
 class CustomDataset(Dataset):
-    def __init__(self, root_dir, image_dir, label_dir, real_image_dir, num_samples, transform=None):
+    def __init__(self, root_dir, image_dir, label_dir, real_image_dir, num_samples, transform=None,consider_real=True):
+        self.consider_real=consider_real
         self.root_dir = root_dir
         self.image_dir = image_dir
         self.label_dir = label_dir
         self.real_image_dir = real_image_dir
         self.transform = transform
         self.image_files = sorted(os.listdir(os.path.join(root_dir, image_dir)))
-        self.real_image_files = sorted(os.listdir(real_image_dir))
+        self.real_image_files = []
+        for dirpath, dirnames, filenames in os.walk(self.real_image_dir):
+            for filename in filenames:
+                if filename.endswith('.jpg') or filename.endswith('.png'):
+                    relative_path = os.path.relpath(os.path.join(dirpath, filename), self.real_image_dir)
+                    self.real_image_files.append(relative_path)
+
         self.label_files = [f for f in sorted(os.listdir(os.path.join(root_dir, label_dir))) if f.endswith('.jpg') or f.endswith('.png')]
         self.num_samples = num_samples
 
@@ -23,24 +30,40 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         # Randomly select an image pair from the synthetic dataset
         synthetic_idx = random.randint(0, len(self.image_files) - 1)
-        image_file = self.image_files[synthetic_idx].split('.')[0].split("_")[0] + '_' + str(random.randint(1, 14)) + '.jpg'
+        print(f"Synthetic: {synthetic_idx}")
+        if(self.image_dir == "/content/rainy-image-dataset/rainy image"):
+          image_file = self.image_files[synthetic_idx].split('.')[0].split("_")[0] + '_' + str(random.randint(1, 14)) + '.jpg'
+        else:
+          image_file = self.image_files[synthetic_idx]
+        # the above image file construction only works when rainy_image_dataset is considered.
+        # it doesnt work for any generalised dataset 
+        print(f"image file: {image_file}")
+        
         image_path = os.path.join(self.root_dir, self.image_dir, image_file)
+        print(f"image_path: {image_path}")
+        
+        
         label_path = os.path.join(self.root_dir, self.image_dir, self.image_files[synthetic_idx])
-
-        # Randomly select a real rainy image
-        real_idx = random.randint(0, len(self.real_image_files) - 1)
-        real_image_file = self.real_image_files[real_idx]
-        real_image_path = os.path.join(self.real_image_dir, real_image_file)
-
+        print(f"label_path: {label_path}")
+        
+        
         # Load and transform the images
         image = Image.open(image_path).convert('RGB')
         label = Image.open(label_path).convert('RGB')
-        real_image = Image.open(real_image_path).convert('RGB')
+        if self.consider_real:
+        # Randomly select a real rainy image
+          real_idx = random.randint(0, len(self.real_image_files) - 1)
+          real_image_file = self.real_image_files[real_idx]
+          real_image_path = os.path.join(self.real_image_dir, real_image_file)
+          real_image = Image.open(real_image_path).convert('RGB')
+
+          if self.transform:
+            real_image = self.transform(real_image)
 
         if self.transform:
             image = self.transform(image)
             label = self.transform(label)
-            real_image = self.transform(real_image)
+        print(real_image_path, image_path, label_path)
 
         return image, label, real_image
 
--- test.py
+++ test.py
@@ -13,33 +13,35 @@ transform = transforms.Compose([
     transforms.Resize((128, 128)),
     transforms.ToTensor(),
 ])
-
+print(transform)
 
 test_dataset = CustomDataset(root_dir='/content/ed-udrdr/rainy', real_image_dir="/content/drive/MyDrive/Machine_Learning/Paper/Custom/GT-RAIN_train",image_dir='rainy', label_dir='d rainy', num_samples=10,transform=transform)
-
+print(test_dataset)
 test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
-
+print(test_loader)
 def test_model(model, test_loader, device):
     model = model.to(device)
     model.eval()  # Set the model to evaluation mode
     criterion = nn.MSELoss()
 
     with torch.no_grad():  # No need to track gradients in testing
-        for i, (rainy_images, clear_images) in enumerate(test_loader):
-            rainy_images = rainy_images.to(device)
-            clear_images = clear_images.to(device)
-
+        for i, (x_syn, y_gt, _) in enumerate(test_loader):
+            x_syn = x_syn.to(device)
+            print(x_syn)
+            y_gt = y_gt.to(device)
+            print(y_gt)
             # Forward pass
-            outputs = model(rainy_images)
+            outputs = model(x_syn)
 
             # Compute loss
-            loss = criterion(outputs, clear_images)
+            loss = criterion(outputs, y_gt)
 
             print(f'Test Step [{i+1}/{len(test_loader)}], Loss: {loss.item()}')
 
 modeldict = torch.load("/content/drive/MyDrive/models_07062024_numsamples100_epoch1_Batchsize8.pth")
-
+print(modeldict)
 model_state_dict = modeldict["DN_state_dict"]
 model = RainEstimationNetwork()
 model.load_state_dict(model_state_dict)
+print(model)
 test_model(model, test_loader, device)
